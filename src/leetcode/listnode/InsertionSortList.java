004b7ac8d93e7ea0f795f668bed155e2387e2709
romantie <2553615406@qq.com>
2020-11-20 15:59:18 +0800

    每日一题，链表的插入排序，害挺不好理解，我好菜

---
+++ src/leetcode/listnode/InsertionSortList.java
@@ -0,0 +1,35 @@
+package leetcode.listnode;
+
+public class InsertionSortList {
+    class ListNode{
+        int val ;
+        ListNode next;
+        ListNode(){}
+        ListNode(int x){val = x;}
+    }
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode pre = dummyHead;
+                while (pre.next.val <= curr.val) {
+                    pre = pre.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = pre.next;
+                pre.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+
+        return dummyHead.next;
+    }
+
+}
