cf29903505257a25d69b3a778e9bad4d8d54263d
romantie <2553615406@qq.com>
2020-11-23 14:39:47 +0800

    三数之和问题，双指针解决

---
+++ src/leetcode/Array/ThreeSum.java
@@ -0,0 +1,42 @@
+package leetcode.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 2020/11/23
+ *
+ */
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> ans = new ArrayList<>();
+        int len = nums.length;
+        if (nums == null || len<3) return ans;
+        Arrays.sort(nums);
+        for (int i =0;i<len;i++){
+            //如果第一个数字大于0，那么三叔之和必定大于0
+            if (nums[i] >0 ) break;
+            //去重
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            //定义两个指针
+            int L=i+1;
+            int R=len-1;
+            while(L<R){
+                int sum = nums[i] + nums[L] +nums[R];
+                if (sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while(L < R && nums[L] == nums[L+1]) L++;
+                    while (L < R && nums[R] == nums[R-1]) R--;
+                    L++;
+                    R--;
+                }
+                else if (sum < 0) L++;
+                else if (sum > 0) R--;
+            }
+
+        }
+        return ans;
+    }
+
+}
