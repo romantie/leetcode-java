48944d8c3b2f97734bd80c029b754ec7e44d7c8b
romantie <2553615406@qq.com>
2020-11-30 09:06:17 +0800

    四数之和，四个数组，增加了注释

--- src/leetcode/hashTable/FourSumCount.java
+++ src/leetcode/hashTable/FourSumCount.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 
 /**
  * 给定几个数组，求出每个数组中相加和为零
+ * 将其分为两组，每组相加后判断正负即可。使用hashMap、
  */
 public class FourSumCount {
     public static int fourSumCount(int[] A ,int[] B,int[] C,int[] D){
+        //创建一个hashMap存储结果
         HashMap<Integer, Integer> countAB = new HashMap<>();
+        //前两个相加，
         for (int u :A){
             for (int v : B){
                 countAB.put(u+v,countAB.getOrDefault(u+v,0)+1);
@@ -15,10 +18,11 @@ public class FourSumCount {
             }
         }
         int ans = 0;
+        //后两个相加然后判断是否和一组的结果相负
         for (int u :C){
             for (int v :D){
                 if (countAB.containsKey(-u-v))
-                ans+=countAB.get(-u-v);
+                    ans+=countAB.get(-u-v);
             }
         }
         return ans;
