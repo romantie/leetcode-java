46db4d158281b7882a577a820ece9ef416485180
romantie <2553615406@qq.com>
2020-12-10 20:52:34 +0800

    括号的生成22题

---
+++ src/leetcode/回溯/括号生成22.java
@@ -0,0 +1,36 @@
+package leetcode.回溯;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2020/12/10括号的生成，给点一个数字n，代表括号的数量
+ * 使用回溯的方法，并且将结果保存数组
+ */
+public class 括号生成22 {
+
+
+    ArrayList[] cache = new ArrayList[100];
+    public List<String> generate(int n){
+        if (cache[n] != null){
+            return cache[n];
+        }
+        ArrayList<String> ans = new ArrayList<String>();
+        if (n == 0){
+            ans.add("");
+        }else{
+            for (int c=0;c<n;++c){
+                for (String left:generate(c)){
+                    for (String right:generate(n-1-c)){
+                        ans.add("(" + left + ")" + right);
+                    }
+                }
+            }
+        }
+        cache[n] = ans;
+        return ans;
+    }
+    public List<String> generateParenthesis(int n){
+        return generate(n);
+    }
+}
