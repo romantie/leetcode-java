c204e27a9901c686a3b25f51ea7fb8b80bac4b1e
romantie <2553615406@qq.com>
2020-12-16 20:23:31 +0800

    上升下降字符串1370，使用桶计数

--- src/leetcode/Array/上升下降字符串1370.java
+++ src/leetcode/Array/上升下降字符串1370.java
@@ -6,21 +6,24 @@ package leetcode.Array;
  * 使用桶计数解决，自己了解桶排序
  *
  */
-public class SortString1370 {
+public class 上升下降字符串1370 {
     public static String sortString(String s){
         int[] bucket = new int[26];
+        //初始化桶，将字符个数存入桶中
         for (int i = 0;i<s.length();i++){
             bucket[s.charAt(i)-'a']++;
         }
 
         StringBuffer stringBuffer = new StringBuffer();
         while(stringBuffer.length()<s.length()){
+            //将字符一一取出，从小到大
             for (int i=0;i<26;i++){
                 if (bucket[i]>0){
                     stringBuffer.append((char)(i+'a'));
                     bucket[i]--;
                 }
             }
+            //将字符从到小取出，
             for (int i = 25;i>=0;i--){
                 if (bucket[i]>0){
                     stringBuffer.append((char)(i+'a'));
