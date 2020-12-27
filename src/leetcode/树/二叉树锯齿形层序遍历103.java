7c64952a60e7102897b3f4e742b7bf1e06f1fc4a
romantie <2553615406@qq.com>
2020-12-27 16:23:36 +0800

    二叉树的锯齿形层序遍历103

---
+++ src/leetcode/树/二叉树锯齿形层序遍历103.java
@@ -0,0 +1,56 @@
+package leetcode.树;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 2020/12/27
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。
+ * 即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行
+ */
+public class 二叉树锯齿形层序遍历103 {
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){val = x;}
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root){
+        //构建一个数组保存最终的结果
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+        //队列保存每一层的节点
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean isLeftOrder = true;
+        while(!queue.isEmpty()){
+            //此双端队列保存每一层的数据
+            LinkedList<Integer> level = new LinkedList<>();
+            int sizeQueue = queue.size();
+            for (int i = 0; i<sizeQueue; i++){
+                TreeNode node = queue.poll();
+                //如果为true，从左开始读取，数据将全部加入尾部，反之，数据全部加入首位。
+                if (isLeftOrder){
+                    level.addLast(node.val);
+                }else {
+                    level.addFirst(node.val);
+                }
+
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            res.add(new LinkedList<>(level));
+            //每一层循环完之后，将isLeftOrder反转，使下一层的方向相反
+            isLeftOrder = !isLeftOrder;
+        }
+        return res;
+
+    }
+}
