de33493b71903c61d67ceb72920e4490297d748c
romantie <2553615406@qq.com>
2020-12-28 18:51:54 +0800

    对称二叉树101

--- src/leetcode/树/对称二叉树101.java
+++ src/leetcode/树/对称二叉树101.java
@@ -1,5 +1,7 @@
 package leetcode.树;
 
+import java.util.LinkedList;
+
 public class 对称二叉树101 {
     private class TreeNode{
         int val;
@@ -31,6 +33,37 @@ public class 对称二叉树101 {
             return false;
         }
         return p.val == q.val && check(p.left,q.right) && check(p.right,q.left);
+    }
+
+
+    //迭代解法，使用队列，将两个指针分别入队，然后使用方法一的思路，
+    public boolean isSymmetric2(TreeNode root){
+        return check2(root,root);
+    }
+    private boolean check2(TreeNode u,TreeNode v){
+        LinkedList<TreeNode> q = new LinkedList<>();
+        //使用两个指针分别指向树的根节点
+        q.offer(u);
+        q.offer(v);
+        //指针向下遍历，
+        while (!q.isEmpty()){
+            u = q.poll();
+            v = q.poll();
+            //如果两个指针指向的节点相同则跳出此次判断，
+            if (u == null && v == null){
+                continue;
+            }
+            if (u == null || v == null || u.val != v.val){
+                return false;
+            }
+            //分别向节点的下一个节点入队
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
+        }
+        return true;
     }
 
 }
