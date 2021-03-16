df3045eeeb8ddd55ae48e1b07578326c5528e651
romantie <2553615406@qq.com>
2021-03-16 16:37:12 +0800

    简单修改

--- src/leetcode/listnode/BasicListNode.java
+++ src/leetcode/listnode/BasicListNode.java
@@ -1,5 +1,7 @@
 package leetcode.listnode;
 
+import java.util.List;
+
 /**
  * 链表的基础练习
  * 获取链表的第index的系欸但的数2.在链表的最前面，最后面，在链表的index前插入一个节点
@@ -33,9 +35,29 @@ public class BasicListNode {
         }
         return curr.val;
     }
+    public int gettwo (int index ,ListNode head){
+        ListNode dummyHead = head;
+        if (index < 0){
+            return -1;
+        }
+        ListNode curr = dummyHead.next;
+        while(curr.next != null && index >-1){
+            curr = curr.next;
+            index--;
+        }
+        return curr.val;
+    }
     //在链表的最前面插入一个值
     public ListNode addAtHead(int val,ListNode head){
         ListNode dummyHead = null,curr = null;
+        dummyHead.next = head;
+        curr.val = val;
+        curr.next = head;
+        dummyHead.next = curr;
+        return dummyHead;
+    }
+    public  ListNode add(int val ,ListNode head){
+        ListNode dummyHead = null, curr = null;
         dummyHead.next = head;
         curr.val = val;
         curr.next = head;
