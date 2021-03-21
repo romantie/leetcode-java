1b1332d1eca9164beee47afa4bfaba6c5d584800
romantie <2553615406@qq.com>
2021-03-21 20:09:49 +0800

    连续的空座位603.sql

--- src/MYSQL/连续的空座位603.sql
+++ src/MYSQL/连续的空座位603.sql
@@ -9,3 +9,21 @@ from cinema a join cinema b
     and a.free = true and b.free = true
 group by a.seat_id
 order by a.seat_id;
+
+##查询近30天的活跃用户数
+select activity_data as day ,count(distinct user_id) as activity_users
+from Activity
+where datediff('2019-07-27',activity_date) < 30
+group by activity_data;
+
+select ifnull(round(count(distinct session_id) / count(distinct user_id),2),0) as k
+from activity
+where datediff('2019-07-27',activity_date) < 30
+
+select ifnull(
+    round(count(distinct session_id) / count(distinct user_id),2),
+    0
+           )as k
+from
+    activity
+where datediff('2017-07-27',activity) < 30;
