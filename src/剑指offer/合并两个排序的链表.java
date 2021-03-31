f2bfc71cad2a7442eca3e09fe18b17604c87c2ba
romantie <2553615406@qq.com>
2021-03-31 21:36:37 +0800

    新增剑指offer练习题

--- src/剑指offer/合并两个排序的链表.java
+++ src/剑指offer/合并两个排序的链表.java
@@ -10,7 +10,7 @@ public class 合并两个排序的链表 {
     public static ListNode mergeTwoLists(ListNode l1,ListNode l2){
         ListNode l3 = new ListNode(0);
         ListNode cur = l3;
-        while (l1.next != null && l2.next != null){
+        while (l1 != null && l2 != null){
             if (l1.val <= l2.val){
                 cur.next = l1;
                 l1 = l1.next;
